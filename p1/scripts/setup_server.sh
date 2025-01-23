3deef443b6013d5dba5af8caf37a70f5fa35369b
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2025-01-23 10:19:23 +0100

    p2 starting

--- p1/scripts/setup_server.sh
+++ p1/scripts/setup_server.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-sudo apt-get update -y
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt install -y curl
 
-curl -sfL https://get.k3s.io | sh -
+cat /home/vagrant/.ssh/me.pub >>/home/vagrant/.ssh/authorized_keys
+mkdir -p /root/.ssh
 
-mkdir -p ~/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-sudo chown $(id -u):$(id -g) ~/.kube/config
+cat /home/vagrant/.ssh/me.pub >>/root/.ssh/authorized_keys
 
-sudo systemctl status k3s
+# Install K3s server
 
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
+  --node-ip=192.168.56.110 \
+  --advertise-address=192.168.56.110 \
+  --node-taint CriticalAddonsOnly=true:NoExecute" sh -
+
+# Save token
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+chmod 644 /vagrant/node-token
+
+sudo kubectl version --client
--- p1/scripts/setup_worker.sh
+++ p1/scripts/setup_worker.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt install -y curl
 
-sudo apt-get update -y
+cat /home/vagrant/.ssh/me.pub >>/home/vagrant/.ssh/authorized_keys
+mkdir -p /root/.ssh
+cat /home/vagrant/.ssh/me.pub >>/root/.ssh/authorized_keys
 
+# Set up K3s worker
+export K3S_URL="https://192.168.56.110:6443"
+export K3S_TOKEN=$(cat /vagrant/node-token)
+export INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1"
 
-K3S_URL="https://192.168.56.110:6443" 
-K3S_TOKEN=""
-curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -
+# Install K3s agent
+curl -sfL https://get.k3s.io | sh -
 
-sudo systemctl status k3s-agent
-
+sudo kubectl version --client
