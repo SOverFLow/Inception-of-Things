9d9b7787c934e726c04c59bbd111dae0eded6d3b
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2025-02-20 18:03:30 +0100

    refactor: :sparkles: change from ubuntu to debian to fix the eth1 problem +  add the k8s image from the docker hub for the 3 apps

--- p1/scripts/setup_server.sh
+++ p1/scripts/setup_server.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt install -y curl
+apt-get install -y curl
 
-# cat /home/vagrant/.ssh/me.pub >>/home/vagrant/.ssh/authorized_keys
-# mkdir -p /root/.ssh
-
-# cat /home/vagrant/.ssh/me.pub >>/root/.ssh/authorized_keys
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
-  --node-ip=192.168.56.110 \
-  --advertise-address=192.168.56.110 \
-  --node-taint CriticalAddonsOnly=true:NoExecute" sh -
+curl -sfL https://get.k3s.io | sh -
 
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 chmod 644 /vagrant/node-token
--- p2/scripts/setup_server.sh
+++ p2/scripts/setup_server.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# export DEBIAN_FRONTEND=noninteractive
 apt-get update
-# apt-get install -y curl
+apt-get install -y curl
 
-# cat /home/vagrant/.ssh/me.pub >>/home/vagrant/.ssh/authorized_keys
-# mkdir -p /root/.ssh
 
-# cat /home/vagrant/.ssh/me.pub >>/root/.ssh/authorized_keys
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
-  --node-ip=192.168.56.110 \
-  --advertise-address=192.168.56.110" sh -
+curl -sfL https://get.k3s.io | sh -
 
 sudo kubectl apply -f ./confs/web-app-1/web-app-1-deployement.yaml
 sudo kubectl apply -f ./confs/web-app-2/web-app-2-deployement.yaml
